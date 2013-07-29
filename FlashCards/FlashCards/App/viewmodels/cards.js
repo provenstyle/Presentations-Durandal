b0cf888f5cda9d81733cb924a3d92f88c32026a6
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-07-29 12:27:00 -0500

    Bringing over the working flashcard application

---
+++ FlashCards/FlashCards/App/viewmodels/cards.js
@@ -0,0 +1,10 @@
+﻿define([], function () {
+
+    var viewAttached = function() {
+        window.scrollTo(0, 1);
+    };
+
+    return {        
+        viewAttached: viewAttached,                
+    };
+});
---
+++ FlashCards/FlashCards/App/viewmodels/shell.js
@@ -0,0 +1,12 @@
+﻿define(['durandal/plugins/router', 'durandal/app'], function (router, app) {
+
+    return {
+        router: router,        
+        activate: function () {
+            //Scroll ios to the header bar
+            //setTimeout(function () { window.scrollTo(0, 1); }, 1000);
+            
+            return router.activate('library');
+        }
+    };
+});
